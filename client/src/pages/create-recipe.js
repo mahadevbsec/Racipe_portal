894f0833658f9298709d15e0aafd0f8ed0ae8df5
mahadevbsec <gsmahadeva52@gmail.com>
2025-04-10 20:06:37 +0530

    Update create-recipe.js

--- client/src/pages/create-recipe.js
+++ client/src/pages/create-recipe.js
@@ -40,7 +40,7 @@ export const CreateRecipe = () => {
     event.preventDefault();
     try {
       await axios.post(
-        "http://localhost:3001/recipes",
+        "https://racipe-portal-kh85.onrender.com/recipes",
         { ...recipe },
         {
           headers: { authorization: cookies.access_token },
